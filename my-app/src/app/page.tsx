068ec8e30d639da6502e284e5cf5a886b0fd5643
Gordon Sheen <gsheen@hawaii.edu>
2025-03-01 12:44:34 -1000

    Task 3

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,57 @@
+"use client";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
+import './style.css';
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Facebook, Twitter, Pinterest, Instagram, HouseFill, Search, PersonFill, Cart } from 'react-bootstrap-icons';
+
+const TopMenu = () => (
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Nav className="me-auto">
+        <Nav.Link><Facebook/></Nav.Link>
+        <Nav.Link><Twitter/></Nav.Link>
+        <Nav.Link><Pinterest/></Nav.Link>
+        <Nav.Link><Instagram/></Nav.Link>
+      </Nav>
+      <Nav className="justify-content-end">
+        <Nav.Link><HouseFill/> </Nav.Link>
+        <Nav.Link><Search/> </Nav.Link>
+        <Nav.Link><PersonFill/> </Nav.Link>
+        <NavDropdown title={<Cart/>}>
+          <NavDropdown.Item></NavDropdown.Item>
+          <NavDropdown.ItemText>Your cart is currently empty.</NavDropdown.ItemText>
+        </NavDropdown>
+      </Nav>
+    </Container>
+  </Navbar>
+);
+
+const IslandSnowLogo = () => (
+  <h1>IslandSnowLogo</h1>
+);
+
+const MiddleMenu = () => (
+  <h1>MiddleMenu</h1>
+);
+
+const FullWidthImage = () => (
+  <h1>FullWidthImage</h1>
+);
+
+const FooterMenu = () => (
+  <h1>FooterMenu</h1>
+);
 
 export default function Home() {
   return (
     <main>
       <Container>
-        <h1 className="text-center">Island Snow!</h1>
+        <TopMenu/>
+        <IslandSnowLogo/>
+        <MiddleMenu/>
+        <FullWidthImage/>
+        <FooterMenu/>
       </Container>
     </main>
   );
